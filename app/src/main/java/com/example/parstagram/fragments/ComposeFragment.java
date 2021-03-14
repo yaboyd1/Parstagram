efa5f6f4fe4c73f2117da90b1a7a67133316020d
Dewan <dtahmid000@citymail.cuny.edu>
2021-03-13 23:12:21 -0500

    RecyclerView for Posts

--- app/src/main/java/com/example/parstagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/ComposeFragment.java
@@ -180,21 +180,4 @@ public class ComposeFragment extends Fragment {
             }
         });
     }
-
-    private void queryPosts() {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Issue with getting posts!", e);
-                    return;
-                }
-                for (Post post : posts) {
-                    Log.i(TAG, "Post: " + post.getDescription() + ", Username: " + post.getUser().getUsername());
-                }
-            }
-        });
-    }
 }
